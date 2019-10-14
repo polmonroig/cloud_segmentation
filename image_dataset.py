3b72ea1aea8bfef752c28bf0b1e3274ec6f95543
Pol Monroig <polmonroig@yahoo.es>
2019-10-14 18:19:14 +0200

    Add split dataset blueprint

--- image_dataset.py
+++ image_dataset.py
@@ -1,9 +1,11 @@
 from torch.utils.data import Dataset
+import pandas as pd
+import numpy as np
 
 
 class ImageDataset(Dataset):
 
-    def __init__(self, root, transform):
+    def __init__(self, root, transform, labels):
         super(ImageDataset, self).__init__()
 
     def __getitem__(self, item):
@@ -11,3 +13,19 @@ class ImageDataset(Dataset):
 
     def __len__(self):
         return 0
+
+
+def get_train_val_set(data_dir, label_dir, val=0.2):
+    encodings = pd.read_csv(label_dir)
+    encodings['Image_Label'] = encodings['Image_Label'].apply(lambda x: x.split('_')[0])
+    msk = np.random.rand(len(encodings)) < (1 - val)
+    print(len(msk))
+    print(len(encodings))
+    train_x = encodings[msk]['Image_Label']
+    print(len(train_x))
+    print("dssd")
+    train_y = encodings[msk]['EncodedPixels']
+    val_x = encodings[~msk]['Image_Label']
+    val_y = encodings[~msk]['EncodedPixels']
+    return train_x, train_y, val_x, val_y
+
--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import wandb
 import torch
 from segNet import SegNet
-from image_dataset import ImageDataset
+from image_dataset import ImageDataset, get_train_val_set
 import utils
+
 
 def main():
 
-    transform = None
-    dataset = ImageDataset(utils.DATA_DIR, transform)
-
-    model = SegNet()
-
+    x_train, y_train, x_val, y_val = get_train_val_set(utils.DATA_DIR, utils.TRAIN_LABELS)
+    print(len(x_val.drop_duplicates()))
+    print(len(x_train.drop_duplicates()))
+    print(len(x_val))
+    print(len(x_train))
 
 
 
